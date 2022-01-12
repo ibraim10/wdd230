b027267ad5bf815d28f807111af6aa6a6cb9f881
Ibraim Calvo <ibraim_10@hotmail.com>
2022-01-11 19:35:29 -0600

    Add style and media query

---
+++ js/time.js
@@ -0,0 +1,7 @@
+// let currentDateAndTime = Date();
+
+// document.write("It is now "+currentDateAndTime);
+
+var string = document.lastModified;
+document.write(string);
+// returns: Tuesday, December 16, 2017 11:09:42
